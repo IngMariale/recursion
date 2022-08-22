51d6d71891b1bcf3cbf48b6a4c2cd3616a425767
IngMariale <4725@holbertonsschool.com>
2022-08-22 13:20:06 -0700

    add strlen function

---
+++ 2-strlen.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+
+ * _strlen - function that returns the length of a string
+
+ * @s: string
+
+ * Return: i, the len of the string
+
+ */
+
+
+
+int _strlen(char *s)
+  
+{
+  
+  int i = 0;
+  
+
+  
+  for (; s[i]; i++)
+    
+    {
+      
+    }
+  
+
+  
+  return (i);
+  
+}
